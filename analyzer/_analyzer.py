3e8e93792612d63c1b005663c885821493ecb867
Nilusink <nilusink@protonmail.com>
2023-12-12 21:46:27 +0100

    added file size and version deconfliction

--- analyzer/_analyzer.py
+++ analyzer/_analyzer.py
@@ -7,11 +7,44 @@ automatically restarts ssh-tunnels
 Author:
 Nilusink
 """
+from dataclasses import dataclass
+from pathlib import Path
 import typing as tp
 import shutil
+import re
 import os
 
 
+# dataclasses
+@dataclass(frozen=False)
+class Mod:
+    name: str
+    version: str
+    folder_name: str
+    size: int
+
+
+@dataclass(frozen=True)
+class ModCollection:
+    name: str
+    versions: list[str]
+    folder_name: str
+    size: int
+
+    def __len__(self) -> int:
+        return len(self.versions)
+
+
+# types
+type mods_collection_t = dict[str, dict[str, ModCollection]]
+type diff_result_t = tuple[
+            list[str],
+            dict[str, list[ModCollection]],
+            dict[str, list[ModCollection]]
+]
+
+
+# helper functions
 def find_uniques[T](a: tp.Iterable[T], b: tp.Iterable[T]) -> list[T]:
     """
     compare two lists and get the unique items from list a
@@ -22,9 +55,65 @@ def find_uniques[T](a: tp.Iterable[T], b: tp.Iterable[T]) -> list[T]:
     return [f for f in a if f not in b]
 
 
+def name_and_version(s: str) -> Mod:
+    """
+    separate mod name and version
+
+    :param s: version structure: a.b.c
+    :return:
+    """
+    try:
+        ver = re.search(r"\d+\.\d+\.\d+", s).group()
+
+    except AttributeError:
+        ver = ""
+
+    return Mod(s.strip(ver).rstrip().lstrip(), ver, s, 0)
+
+
+def max_version(*versions: str) -> str:
+    """
+    gets the max version
+    """
+    max_v = (0, 0, 0)
+
+    # TODO: probably a much cleaner way to do this
+    for version in versions:
+        a, b, c = [int(n) for n in version.split(".")]
+
+        if a > max_v[0]:
+            max_v = (a, b, c)
+
+        elif a == max_v[0]:
+            if b > max_v[1]:
+                max_v = (a, b, c)
+
+            elif b == max_v[1]:
+                if c > max_v[2]:
+                    max_v = (a, b, c)
+
+    return ".".join([str(n) for n in max_v])
+
+
+def get_directory_size(directory: str) -> int:
+    """
+    get the size of a directory
+
+        credits: https://stackoverflow.com/users/24718/monkut
+
+    :param directory: starting directory
+    """
+    root_directory = Path(directory)
+    return sum(
+        f.stat().st_size for f in root_directory.glob('**/*') if f.is_file()
+    )
+
+
 class Analyzer:
     _directory: str
-    _mods_per_sub: dict[str, list[str]]
+
+    # structure: {"sub_module": {"mod": <Mod: name, version, folder_name}}
+    _mods_per_sub: mods_collection_t
 
     def __init__(self, directory: str) -> None:
         self._directory = directory
@@ -36,7 +125,7 @@ class Analyzer:
         return self._directory
 
     @property
-    def mods_per_sub(self) -> dict[str, list[str]]:
+    def mods_per_sub(self) -> mods_collection_t:
         return self._mods_per_sub.copy()
 
     def parse(self) -> None:
@@ -59,23 +148,38 @@ class Analyzer:
                 print(f"no mods found in \"{sub_folder}\"")
                 continue
 
-            self._mods_per_sub[sub_folder] = mods
+            self._mods_per_sub[sub_folder] = {}
+
+            for mod in mods:
+                vmod = name_and_version(mod)
+                vmod.size = get_directory_size(os.path.join(
+                    self._directory,
+                    sub_folder,
+                    vmod.folder_name
+                ))
+
+                if vmod.name in self._mods_per_sub[sub_folder]:
+                    self._mods_per_sub[sub_folder][vmod.name].versions.append(
+                        vmod.version
+                    )
+                    continue
+
+                self._mods_per_sub[sub_folder][vmod.name] = ModCollection(
+                    vmod.name, [vmod.version], vmod.folder_name, vmod.size
+                )
 
     def diff(
             self, other: tp.Self,
             print_results: bool = False
-    ) -> tuple[
-        tuple[list[str], dict[str, list[str]]],
-        tuple[list[str], dict[str, list[str]]]
-    ]:
+    ) -> tuple[diff_result_t, diff_result_t]:
         """
         compare two directories
 
         :param other: the Analyzer instance to compare against
         :param print_results: whether to print the results of the analysis
 
-        :returns: (unique sub-folders, unique mods) to self,
-            (unique sub-folders, unique mods) to other
+        :returns: (unique sub-folders, unique mods, duplicates) to self,
+            (unique sub-folders, unique mods, duplicates) to other
         """
         own_sub_folders = self.mods_per_sub.keys()
         other_sub_folders = other.mods_per_sub.keys()
@@ -85,24 +189,65 @@ class Analyzer:
 
         common_folders = list(set(own_sub_folders) & set(other_sub_folders))
 
-        own_unique_per_sub: dict[str, list[str]] = {
+        # find duplicates
+        own_duplicates: dict[str, list[ModCollection]] = {
             sub: [] for sub in common_folders
         }
-        other_unique_per_sub: dict[str, list[str]] = {
+        other_duplicates: dict[str, list[ModCollection]] = {
             sub: [] for sub in common_folders
         }
+
+        for sub_folder in own_sub_folders:
+            for mod in self.mods_per_sub[sub_folder]:
+                if len(self.mods_per_sub[sub_folder][mod].versions) > 1:
+                    own_duplicates[sub_folder].append(
+                        self.mods_per_sub[sub_folder][mod]
+                    )
+
+        for sub_folder in other_sub_folders:
+            for mod in other.mods_per_sub[sub_folder]:
+                if len(other.mods_per_sub[sub_folder][mod].versions) > 1:
+                    other_duplicates[sub_folder].append(
+                        other.mods_per_sub[sub_folder][mod]
+                    )
 
         if print_results:
             print(f"unique (self): {own_sub_unique}")
             print(f"unique (other): {other_sub_unique}")
             print(f"common: {common_folders}")
 
+        # cross-match mods
+        own_unique_per_sub: dict[str, list[ModCollection]] = {
+            sub: [] for sub in common_folders
+        }
+        other_unique_per_sub: dict[str, list[ModCollection]] = {
+            sub: [] for sub in common_folders
+        }
         for sub_folder in common_folders:
             own_mods = self.mods_per_sub[sub_folder]
             other_mods = other.mods_per_sub[sub_folder]
 
-            own_mods_unique = find_uniques(own_mods, other_mods)
-            other_mods_unique = find_uniques(other_mods, own_mods)
+            # find uniques (using the folders name)
+            tmp_own_mods_unique = find_uniques(
+                [mod.name for mod in own_mods.values()],
+                [mod.name for mod in other_mods.values()]
+            )
+            tmp_other_mods_unique = find_uniques(
+                [mod.name for mod in other_mods.values()],
+                [mod.name for mod in own_mods.values()]
+            )
+
+            # replace the name with the classes
+            own_mods_unique: list = []
+            other_mods_unique: list = []
+
+            for mod in own_mods.values():
+                if mod.name in tmp_own_mods_unique:
+                    own_mods_unique.append(mod)
+
+            for mod in other_mods.values():
+                if mod.name in tmp_other_mods_unique:
+                    other_mods_unique.append(mod)
 
             own_unique_per_sub[sub_folder] = own_mods_unique
             other_unique_per_sub[sub_folder] = other_mods_unique
@@ -114,19 +259,56 @@ class Analyzer:
                 )
 
         return (
-            (own_sub_unique, own_unique_per_sub),
-            (other_sub_unique, other_unique_per_sub)
+            (own_sub_unique, own_unique_per_sub, own_duplicates),
+            (other_sub_unique, other_unique_per_sub, other_duplicates)
         )
+
+    def delete_duplicates(self, other: tp.Self) -> None:
+        """
+        delete all duplicates
+        """
+        (*_, own_duplicates), (*_, other_duplicates) = self.diff(other)
+        other_duplicates: dict[str, list[ModCollection]]
+        own_duplicates: dict[str, list[ModCollection]]
+
+        for sub_folder, mods in own_duplicates.items():
+            for mod in mods:
+                highest_version = max_version(*mod.versions)
+
+                for version in mod.versions:
+                    if version != highest_version:
+                        folder_path = os.path.join(
+                            self.directory,
+                            sub_folder,
+                            mod.folder_name.replace(mod.versions[0], version)
+                        )
+
+                        print(f"deleting {folder_path}")
+                        shutil.rmtree(folder_path)
+
+        for sub_folder, mods in other_duplicates.items():
+            for mod in mods:
+                highest_version = max_version(*mod.versions)
+
+                for version in mod.versions:
+                    if version != highest_version:
+                        folder_path = os.path.join(
+                            other.directory,
+                            sub_folder,
+                            mod.folder_name.replace(mod.versions[0], version)
+                        )
+
+                        print(f"deleting {folder_path}")
+                        shutil.rmtree(folder_path)
 
     def resolve(self, other: tp.Self) -> None:
         """
         resolve conflicts
-        :param other:
         """
         own_changes, other_changes = self.diff(other)
 
-        own_unique_sub, own_unique_mods = own_changes
-        other_unique_sub, other_unique_mods = other_changes
+        own_unique_sub, own_unique_mods, _ = own_changes
+        other_unique_sub, other_unique_mods, _ = other_changes
 
         # copy unique sub-folders
         for sub in own_unique_sub:
@@ -146,16 +328,42 @@ class Analyzer:
         # copy mods
         for sub_folder, mods in own_unique_mods.items():
             for mod in mods:
-                origin = os.path.join(self.directory, sub_folder, mod)
-                dest = os.path.join(other.directory, sub_folder, mod)
+                origin = os.path.join(
+                    self.directory,
+                    sub_folder,
+                    mod.folder_name
+                )
+                dest = os.path.join(
+                    other.directory,
+                    sub_folder,
+                    mod.folder_name
+                )
 
-                print(f"copying \"{origin}\" to \"{dest}\"")
+                print(
+                    f"copying \"{origin}\" to \"{dest}\" "
+                    f"(size: {round(mod.size / (1024**3), 2)} GB)"
+                )
                 shutil.copytree(origin, dest)
 
         for sub_folder, mods in other_unique_mods.items():
             for mod in mods:
-                origin = os.path.join(other.directory, sub_folder, mod)
-                dest = os.path.join(self.directory, sub_folder, mod)
+                origin = os.path.join(
+                    other.directory,
+                    sub_folder,
+                    mod.folder_name
+                )
+                dest = os.path.join(
+                    self.directory,
+                    sub_folder,
+                    mod.folder_name
+                )
 
-                print(f"copying \"{origin}\" to \"{dest}\"")
+                print(
+                    f"copying \"{origin}\" to \"{dest}\" "
+                    f"(size: {round(mod.size / (1024**3), 2)} GB)"
+                )
                 shutil.copytree(origin, dest)
+
+
+if __name__ == "__main__":
+    print(max_version("1.1.0", "0.1.0", "1.2.0", "2.0.0", "2.0.1"))
--- main.py
+++ main.py
@@ -22,8 +22,11 @@ def main() -> int:
     dcs_valid = True
     usb_valid = True
 
-    dcs_directory: str = input("DCS Saved Games Directory: ")
-    usb_directory: str = input("Mods USB Directory: ")
+    # dcs_directory: str = input("DCS Saved Games Directory: ")
+    # usb_directory: str = input("Mods USB Directory: ")
+
+    dcs_directory: str = r"C:\Users\Nilusink\Saved Games\DCS.openbeta\Mods"
+    usb_directory: str = r"E:\Mods"
 
     # check if directories exist
     if not os.path.exists(dcs_directory):
@@ -38,28 +41,91 @@ def main() -> int:
         dcs = Analyzer(dcs_directory)
         usb = Analyzer(usb_directory)
 
+        print("parsing DCS...")
         dcs.parse()
+
+        print("parsing USB...")
         usb.parse()
 
         # compare
-        res = dcs.diff(usb)
-        (dcs_sub_diff, dcs_mod_diff), (usb_sub_diff, usb_mod_diff) = res
+        res = dcs.diff(usb, False)
+        (dcs_sub_diff, dcs_mod_diff, dcs_duplicates) = res[0]
+        (usb_sub_diff, usb_mod_diff, usb_duplicates) = res[1]
+
+        # print duplicates
+        print(f"\n\n##### analysis result #####\n")
+        if sum(
+                [len(mod) for sub in dcs_duplicates for mod in dcs_duplicates[sub]]
+                + [len(mod) for sub in usb_duplicates for mod in usb_duplicates[sub]]
+        ) > 0:
+            print(f"##### duplicates #####\n")
+            print("(DCS)")
+            for sub in dcs_duplicates:
+                print(f"\t* {sub}")
+                for mod in dcs_duplicates[sub]:
+                    print(f"{CLR_BLUE}\t\t- {mod.name} "
+                          f"({', '.join(mod.versions)}){CLR_RESET}")
+
+            print("\n(USB)")
+            for sub in usb_duplicates:
+                print(f"\t* {sub}")
+                for mod in usb_duplicates[sub]:
+                    print(f"{CLR_BLUE}\t\t- {mod.name} "
+                          f"({', '.join(mod.versions)}){CLR_RESET}")
+
+            while True:
+                ans = input("\nAutomatically resolve duplicates? (y/n): ")
+
+                if ans.lower() == "y":
+                    print("\ndeleting duplicates...")
+                    dcs.delete_duplicates(usb)
+
+                    # re-run the analysis
+                    print("parsing DCS...")
+                    dcs.parse()
+
+                    print("parsing USB...")
+                    usb.parse()
+
+                    res = dcs.diff(usb, False)
+                    (dcs_sub_diff, dcs_mod_diff, dcs_duplicates) = res[0]
+                    (usb_sub_diff, usb_mod_diff, usb_duplicates) = res[1]
+
+                    print(f"\n{CLR_GREEN}duplicates resolved "
+                          f"successfully!{CLR_RESET}")
+                    break
+
+                elif ans.lower() == "n":
+                    print("leaving... ")
+                    break
+
+                else:
+                    print("please type either \"y\" to continue, "
+                          "or \"n\" to cancel")
 
         # print results
-        print(f"\n\n##### analysis result #####\n")
+        # print unique folders
+        print(f"\n\n##### unsynced #####\n")
         for sub in dcs_sub_diff:
             print(f"{CLR_GREEN}\t+ {sub}{CLR_RESET}")
 
         for sub in usb_sub_diff:
             print(f"{CLR_RED}\t- {sub}{CLR_RESET}")
 
+        # print mod differences
         for sub in dcs_mod_diff.keys():
             print(f"\t* {sub}")
             for mod in dcs_mod_diff[sub]:
-                print(f"{CLR_GREEN}\t\t+ {mod}{CLR_RESET}")
+                print(
+                    f"{CLR_GREEN}\t\t+ {mod.name} "
+                    f"({round(mod.size / (1024**3), 2)} GB){CLR_RESET}"
+                )
 
             for mod in usb_mod_diff[sub]:
-                print(f"{CLR_RED}\t\t- {mod}{CLR_RESET}")
+                print(
+                    f"{CLR_RED}\t\t- {mod.name} "
+                    f"({round(mod.size / (1024**3), 2)} GB){CLR_RESET}"
+                )
 
         if len(dcs_sub_diff) + len(usb_sub_diff) < 1:
             if all([
@@ -69,8 +135,17 @@ def main() -> int:
                 print(f"\n{CLR_GREEN}Everything synced!{CLR_RESET}")
                 return 0
 
+        total_changed_size = sum([
+            *[mod.size for sub in dcs_mod_diff for mod in usb_mod_diff[sub]],
+            *[mod.size for sub in usb_sub_diff for mod in usb_sub_diff[sub]],
+        ])
+
         while True:
-            ans = input("\nAutomatically resolve your changes? (y/n): ")
+            ans = input(
+                f"\nAutomatically resolve your changes "
+                f"(total: {round(total_changed_size / (1024**3), 2)} GB)? "
+                f"(y/n): "
+            )
 
             if ans.lower() == "y":
                 print("\nstarting copy")
@@ -83,7 +158,10 @@ def main() -> int:
                 return 0
 
             else:
-                print("please type either \"y\" to continue, or \"n\" to cancel")
+                print("please type either \"y\" to continue, "
+                      "or \"n\" to cancel")
+
+    return 1
 
 
 if __name__ == "__main__":
@@ -91,3 +169,6 @@ if __name__ == "__main__":
 
     input("\n\nPress enter to exit")
     exit(rc)
+
+
+# TODO: tkinter filedialog
